7d60618b93e0e5f980bf5c3cbca1b7fdf1d7f838
Damian <dkowalski.1997@gmail.com>
2024-09-01 20:13:44 +0200

    added menu and icons

--- apps/t-vision-angular/src/app/features/dashboard/components/dashboard-menu/dashboard-menu.component.ts
+++ apps/t-vision-angular/src/app/features/dashboard/components/dashboard-menu/dashboard-menu.component.ts
@@ -1,9 +1,11 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  signal,
   ViewEncapsulation,
 } from '@angular/core';
 import { ClassBinder } from '@t-vision/utils';
+import { IconComponent } from '@t-vision/ui';
 
 @Component({
   standalone: true,
@@ -13,8 +15,11 @@ import { ClassBinder } from '@t-vision/utils';
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [ClassBinder],
+  imports: [IconComponent],
 })
 export class DashboardMenuComponent {
+  public items = signal([{ icon: 'home' }, { icon: 'user' }, { icon: 'cog' }]);
+
   constructor(classBinder: ClassBinder) {
     classBinder.bind('tv-dashboard-menu');
   }
