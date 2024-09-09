7d6bb6d5881cb4a18564e3cd6b7cf777e64eb3d5
Damian Kowalski <dkowalski.1997@gmail.com>
2024-09-10 00:09:01 +0200

    added package find controller route

--- apps/t-vision/src/modules/packages/package.service.ts
+++ apps/t-vision/src/modules/packages/package.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Package } from "./entities/package.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class PackageService {
-  public async getPackages(): Promise<any> {
-    return '';
+  constructor(
+    @InjectRepository(Package) private _repository: Repository<Package>
+  ) {}
+
+  public async getPackages(): Promise<Package[]> {
+    return this._repository.find();
   }
 }
